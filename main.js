6a0123bf6ff6d2806dc322148450caef93c17ef5
Gonzalo992 <70953921+Gonzalo992@users.noreply.github.com>
2021-07-14 10:27:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -37,23 +37,4 @@ btnGrabarTexto.addEventListener('click', ()=>{
 
 
 
-/*comandos de voz*/
-if (annyang) {
-  var comandos = {
-    hola: function() {
-      alert("Hola bro");
-    },
-    reproducir: reproducirVideo,
-    pausar: pausarVideo
-  };
-  annyang.addCommands(comandos);
-  annyang.setLanguage("es-MX");
-  annyang.start();
-}
-var video = document.getElementById("mivideo");
-function reproducirVideo() {
-  video.play();
-}
-function pausarVideo() {
-  video.pause();
-}
+
